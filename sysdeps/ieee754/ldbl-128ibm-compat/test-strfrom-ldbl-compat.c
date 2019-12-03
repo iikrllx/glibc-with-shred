jac0bwang/glibc

ldbl-128ibm-compat: Add tests for strfroml, strtold, and wcstold
Since the commit

commit 86a0f56158bd9cbaf2d640e2e6c66539f4cbbcc1
Author: Rajalakshmi Srinivasaraghavan <raji@linux.vnet.ibm.com>
Date:   Thu Jun 28 13:57:50 2018 +0530

    ldbl-128ibm-compat: Introduce ieee128 symbols

IEEE long double versions of strfroml, strtold, and wcstold have been
prepared, but not exposed (which will only happen when the full support
for IEEE long double is complete).  This patch adds tests for these
functions in both IBM and IEEE long double mode.

	* sysdeps/ieee754/ldbl-128ibm-compat/Makefile
	[subdir == stdlib] (tests-internal): Add test-strfrom-ibm128 and
	test-strfrom-ieee128.
	[subdir == wcsmbs] (tests-internal): Add test-wcstold-ibm128 and
	test-wcstold-ieee128.
	(CFLAGS-test-strfrom-ieee128.c, CLAGS-test-strfrom-ibm128.c)
	(CFLAGS-test-wcstold-ieee128.c, CLAGS-test-wcstold-ibm128.c):
	New variables.
	($(objpfx)test-strfrom-ieee128): New rule.
	($(objpfx)test-wcstold-ieee128): Likewise.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-strfrom-ibm128.c:
	New file.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-strfrom-ieee128.c:
	Likewise.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-strfrom-ldbl-compat.c:
	Likewise.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-wcstold-ibm128.c:
	Likewise.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-wcstold-ieee128.c:
	Likewise.
	* sysdeps/ieee754/ldbl-128ibm-compat/test-wcstold-ldbl-compat.c:
	Likewise.

sysdeps/ieee754/ldbl-128ibm-compat/test-strfrom-ldbl-compat.c
@@ -1,0 +1,50 @@
+/* Test for the long double variants of strfroml and strtold.
+   Copyright (C) 2019 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+
+#include <support/check.h>
+
+static int
+do_test (void)
+{
+  int written;
+  char buffer[64];
+  char *bufptr = buffer;
+  const char *expected = "-1.0000000000";
+  long double read;
+
+  /* Write to the buffer.  */
+  written = strfroml (bufptr, 64, "%.10f", (long double) -1);
+  if (written < 0)
+    support_record_failure ();
+
+  /* Compare against the expected output.  */
+  TEST_COMPARE_STRING (expected, buffer);
+
+  /* Read from the buffer.  */
+  read = strtold (expected, NULL);
+
+  /* TODO: Implement and use TEST_COMPARE_FLOAT.  */
+  if (read != (long double) -1)
+    support_record_failure ();
+
+  return 0;
+}
+
+#include <support/test-driver.c>

iikrllx/glibc-with-shred

Reviewed-by: Paul E. Murphy <murphyp@linux.ibm.com>

sysdeps/ieee754/ldbl-128ibm-compat/test-strfrom-ldbl-compat.c
@@ -1,0 +1,49 @@
+/* Test for the long double variants of strfroml and strtold.
+   Copyright (C) 2019 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+
+#include <support/check.h>
+
+static int
+do_test (void)
+{
+  int written;
+  char buffer[64];
+  char *bufptr = buffer;
+  const char *expected = "-1.0000000000";
+  long double read;
+
+  /* Write to the buffer.  */
+  written = strfroml (bufptr, 64, "%.10f", (long double) -1);
+  if (written < 0)
+    support_record_failure ();
+
+  /* Compare against the expected output.  */
+  TEST_COMPARE_STRING (expected, buffer);
+
+  /* Read from the buffer.  */
+  read = strtold (expected, NULL);
+
+  if (read != -1.0L)
+    support_record_failure ();
+
+  return 0;
+}
+
+#include <support/test-driver.c>