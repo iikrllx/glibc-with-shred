bminor/glibc

commit d2ba3677da7a785556fcd708404d8e049b1c063b

powerpc: Add support for POWER10
1. Add the directories to hold POWER10 files.

2. Add support to select POWER10 libraries based on AT_PLATFORM.

3. Let submachine=power10 be set automatically.

sysdeps/powerpc/dl-procinfo.h
@@ -37,7 +37,7 @@
 #define HWCAP_IMPORTANT		(PPC_FEATURE_HAS_ALTIVEC \
 				+ PPC_FEATURE_HAS_DFP)
 
-#define _DL_PLATFORMS_COUNT	15
+#define _DL_PLATFORMS_COUNT	16
 
 #define _DL_FIRST_PLATFORM	32
 /* Mask to filter out platforms.  */
@@ -60,6 +60,7 @@
 #define PPC_PLATFORM_PPC476		12
 #define PPC_PLATFORM_POWER8		13
 #define PPC_PLATFORM_POWER9		14
+#define PPC_PLATFORM_POWER10		15
 
 static inline const char *
 __attribute__ ((unused))
@@ -91,6 +92,14 @@ _dl_string_platform (const char *str)
       str += 5;
       switch (*str)
 	{
+	case '1':
+	  if (str[1] == '0')
+	    {
+	      ret = _DL_FIRST_PLATFORM + PPC_PLATFORM_POWER10;
+	    }
+	  else
+	    return -1;
+	  break;
 	case '4':
 	  ret = _DL_FIRST_PLATFORM + PPC_PLATFORM_POWER4;
 	  break;

sysdeps/powerpc/powerpc32/power10/Implies
@@ -1,0 +1,2 @@
+powerpc/powerpc32/power9/fpu
+powerpc/powerpc32/power9

sysdeps/powerpc/powerpc32/power10/fpu/multiarch/Implies
@@ -1,0 +1,1 @@
+powerpc/powerpc32/power9/fpu/multiarch

sysdeps/powerpc/powerpc32/power10/multiarch/Implies
@@ -1,0 +1,1 @@
+powerpc/powerpc32/power9/multiarch

sysdeps/powerpc/powerpc64/be/power10/Implies
@@ -1,0 +1,2 @@
+powerpc/powerpc64/be/power9/fpu
+powerpc/powerpc64/be/power9

sysdeps/powerpc/powerpc64/be/power10/fpu/Implies
@@ -1,0 +1,1 @@
+powerpc/powerpc64/be/power9/fpu

sysdeps/powerpc/powerpc64/be/power10/fpu/multiarch/Implies
@@ -1,0 +1,1 @@
+powerpc/powerpc64/be/power9/fpu/multiarch

sysdeps/powerpc/powerpc64/be/power10/multiarch/Implies
@@ -1,0 +1,1 @@
+powerpc/powerpc64/be/power9/multiarch

iikrllx/glibc-with-shred

commit 0518c85df59aa1638f18daaf86713be5845494f8

powerpc: Fix POWER10 selection
Add a line that was missing from a previous commit.
Without increasing str, the null-byte is not validated, and
_dl_string_platform returns -1.

Fixes: d2ba3677da7a ("powerpc: Add support for POWER10")

Reviewed-by: Carlos O'Donell <carlos@redhat.com>

sysdeps/powerpc/dl-procinfo.h
@@ -96,6 +96,7 @@ _dl_string_platform (const char *str)
 	  if (str[1] == '0')
 	    {
 	      ret = _DL_FIRST_PLATFORM + PPC_PLATFORM_POWER10;
+	      str++;
 	    }
 	  else
 	    return -1;