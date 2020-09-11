MIPS/glibc

linux: Consolidate lxstat{64}
The LFS support is implemented on lxstat64.c, instead of lxstat.c for
64-bit architectures.  The xstat.c implements the non-LFS and it is
a no-op for !XSTAT_IS_XSTAT64.

The generic non-LFS implementation handles two cases:

  1. New kABIs which uses generic pre 64-bit time Linux ABI (csky and
     nios): it issues __NR_fstat64 with AT_SYMLINK_NOFOLLOW plus handles
     the possible overflow off st_ino, st_size, or st_blocks.  It only
     handles _STAT_VER_KERNEL.

  2. Old KABIs with old non-LFS support (arm, i386, hppa, m68k,
     microblaze, s390, sh, powerpc, and sparc32).  For _STAT_VER_KERNEL
     it issues __NR_lstat, otherwise it isseus __NR_lstat64 and convert
     to non-LFS stat struct and handle possible overflows on st_ino,
     st_size, or st_blocks.

Also non-LFS mips is an outlier and it has its own implementation since
_STAT_VER_LINUX requires a different conversion function (it uses the
kernel_stat as the syscall argument since its exported ABI is different
than the kernel one for both non-LFS and LFS implementation).

The generic LFS implementation handles multiple cases:

  1. XSTAT_IS_XSTAT64 being 1:

    1.1. Old 64-bit kABI (ia64, powerpc64*, s390x, sparc64, x86_64): it
         issues __NR_lstat for _STAT_VER_KERNEL or _STAT_VER_LINUX.

    1.2. New kABIs which uses generic 64-bit Linux ABI (aarch64 and
         riscv64): it issues __NR_newfstatat with AT_SYMLINK_NOFOLLOW and
         only for _STAT_VER_KERNEL.

    1.3. New 32-bit kABIs with only 64-bit time_t support (arc and
         riscv32): it issues __NR_statx and covert to struct stat64.

  2. Old ABIs with XSTAT_IS_XSTAT64 being 0:

    2.1. New kABIs which uses generic pre 64-bit time Linux ABI (csky
	 and nios2): it issues __NR_fstatat64 for _STAT_VER_KERNEL.

    2.2. Old kABIs with old non-LFS support (arm, i386, hppa, m68k,
	 microblaze, s390, sh, mips32, powerpc32, and sparc32): it
	 issues __NR_lstat64.

Also, two special cases requires specific

sysdeps/unix/sysv/linux/lxstat.c
@@ -1,4 +1,4 @@
-/* lxstat using old-style Unix lstat system call.
+/* lxstat using old-style Unix stat system call.
    Copyright (C) 1991-2020 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
@@ -16,45 +16,47 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-/* Ho hum, if xstat == xstat64 we must get rid of the prototype or gcc
-   will complain since they don't strictly match.  */
-#define __lxstat64 __lxstat64_disable
-
-#include <errno.h>
-#include <stddef.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <kernel_stat.h>
-
 #include <sysdep.h>
-#include <sys/syscall.h>
 
-#include <xstatconv.h>
+#if !XSTAT_IS_XSTAT64
+# include <xstatconv.h>
+# include <xstatover.h>
 
 /* Get information about the file NAME in BUF.  */
 int
 __lxstat (int vers, const char *name, struct stat *buf)
 {
-  if (vers == _STAT_VER_KERNEL)
-    return INLINE_SYSCALL (lstat, 2, name, buf);
+  switch (vers)
+    {
+    case _STAT_VER_KERNEL:
+      {
+# if STAT_IS_KERNEL_STAT
+	/* New kABIs which uses generic pre 64-bit time Linux ABI,
+	   e.g. csky, nios2  */
+	int r = INLINE_SYSCALL_CALL (fstatat64, AT_FDCWD, name, buf,
+				     AT_SYMLINK_NOFOLLOW);
+	return r ?: stat_overflow (buf);
+# else
+	/* Old kABIs with old non-LFS support, e.g. arm, i386, hppa, m68k,
+	   microblaze, s390, sh, powerpc, and sparc.  */
+	return INLINE_SYSCALL_CALL (lstat, name, buf);
+# endif
+      }
 
-#if STAT_IS_KERNEL_STAT
-  return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
-#else
-  struct kernel_stat kbuf;
-  int result;
-
-  result = INLINE_SYSCALL (lstat, 2, name, &kbuf);
-  if (result == 0)
-    result = __xstat_conv (vers, &kbuf, buf);
-
-  return result;
+    default:
+      {
+# if STAT_IS_KERNEL_STAT
+	return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
+# else
+	struct stat64 buf64;
+	int r = INTERNAL_SYSCALL_CALL (lstat64, name, &buf64);
+	return r ?: __xstat32_conv (vers, &buf64, buf);
 #endif
+      }
+    }
 }
-
 hidden_def (__lxstat)
-weak_alias (__lxstat, _lxstat);
-#if XSTAT_IS_XSTAT64
-#undef __lxstat64
-strong_alias (__lxstat, __lxstat64);
-hidden_ver (__lxstat, __lxstat64)
-#endif
+#endif /* XSTAT_IS_XSTAT64  */

iikrllx/glibc-with-shred

    1.2. Old 64-bit kABI with defines __NR_lstat64 instead of __NR_lstat
         (sparc64): it issues __NR_lstat for _STAT_VER_KERNEL or
         __NR_lstat64 and convert to struct stat64.
    1.3. New kABIs which uses generic 64-bit Linux ABI (aarch64 and
         riscv64): it issues __NR_newfstatat with AT_SYMLINK_NOFOLLOW
	 and only for _STAT_VER_KERNEL.
    1.4. New 32-bit kABIs with only 64-bit time_t support (arc and
Reviewed-by: Lukasz Majewski <lukma@denx.de>

sysdeps/unix/sysv/linux/lxstat.c
@@ -1,4 +1,4 @@
-/* lxstat using old-style Unix lstat system call.
+/* lxstat using old-style Unix stat system call.
    Copyright (C) 1991-2020 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
@@ -16,45 +16,47 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-/* Ho hum, if xstat == xstat64 we must get rid of the prototype or gcc
-   will complain since they don't strictly match.  */
-#define __lxstat64 __lxstat64_disable
-
-#include <errno.h>
-#include <stddef.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <kernel_stat.h>
-
 #include <sysdep.h>
-#include <sys/syscall.h>
 
-#include <xstatconv.h>
+#if !XSTAT_IS_XSTAT64
+# include <xstatconv.h>
+# include <xstatover.h>
 
 /* Get information about the file NAME in BUF.  */
 int
 __lxstat (int vers, const char *name, struct stat *buf)
 {
-  if (vers == _STAT_VER_KERNEL)
-    return INLINE_SYSCALL (lstat, 2, name, buf);
+  switch (vers)
+    {
+    case _STAT_VER_KERNEL:
+      {
+# if STAT_IS_KERNEL_STAT
+	/* New kABIs which uses generic pre 64-bit time Linux ABI,
+	   e.g. csky, nios2  */
+	int r = INLINE_SYSCALL_CALL (fstatat64, AT_FDCWD, name, buf,
+				     AT_SYMLINK_NOFOLLOW);
+	return r ?: stat_overflow (buf);
+# else
+	/* Old kABIs with old non-LFS support, e.g. arm, i386, hppa, m68k,
+	   microblaze, s390, sh, powerpc, and sparc.  */
+	return INLINE_SYSCALL_CALL (lstat, name, buf);
+# endif
+      }
 
-#if STAT_IS_KERNEL_STAT
-  return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
-#else
-  struct kernel_stat kbuf;
-  int result;
-
-  result = INLINE_SYSCALL (lstat, 2, name, &kbuf);
-  if (result == 0)
-    result = __xstat_conv (vers, &kbuf, buf);
-
-  return result;
+    default:
+      {
+# if STAT_IS_KERNEL_STAT
+	return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
+# else
+	struct stat64 buf64;
+	int r = INLINE_SYSCALL_CALL (lstat64, name, &buf64);
+	return r ?: __xstat32_conv (vers, &buf64, buf);
 #endif
+      }
+    }
 }
-
 hidden_def (__lxstat)
-weak_alias (__lxstat, _lxstat);
-#if XSTAT_IS_XSTAT64
-#undef __lxstat64
-strong_alias (__lxstat, __lxstat64);
-hidden_ver (__lxstat, __lxstat64)
-#endif
+#endif /* XSTAT_IS_XSTAT64  */