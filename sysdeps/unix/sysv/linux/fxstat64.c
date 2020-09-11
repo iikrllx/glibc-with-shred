MIPS/glibc

linux: Consolidate fxstat{64}
The LFS support is implemented on fxstat64.c, instead of fxstat.c for
64-bit architectures.  The fxstat.c implements the non-LFS and it is
a no-op for !XSTAT_IS_XSTAT64.

The generic non-LFS implementation handles two cases:

  1. New kABIs which uses generic pre 64-bit time Linux ABI (csky and
     nios): it issuess __NR_fstat64 plus handle the overflow on st_ino,
     st_size, or st_blocks.  It only handles _STAT_VER_KERNEL.

  2. Old KABIs with old non-LFS support (arm, i386, hppa, m68k,
     microblaze, s390, sh, powerpc, and sparc32).  For _STAT_VER_KERNEL
     it issues __NR_fstat, otherwise it calls __NR_fstat64 and convert
     to non-LFS stat struct and handle possible overflows on st_ino,
     st_size, or st_blocks.

Also non-LFS mips is an outlier and it has its own implementation since
_STAT_VER_LINUX requires a different conversion function (it uses the
kernel_stat as the sysissues argument since its exported ABI is
different than the kernel one for both non-LFS and LFS implementation).

The generic LFS implementation handles multiple cases:

  1. XSTAT_IS_XSTAT64 being 1:

    1.1. 64-bit kABI (aarch64, ia64, powerpc64*, s390x, sparc64,
	 riscv64, and x86_64): it issuess __NR_fstat for
	 _STAT_VER_KERNEL or _STAT_VER_LINUX.

    1.2. New 32-bit kABIs with only 64-bit time_t support (arc and
	 riscv32): it issuess __NR_statx and covert to struct stat64.

  2. Old ABIs with XSTAT_IS_XSTAT64 being 0 (arm, csky, i386, hppa,
     m68k, microblaze, mips32, nios2, sh, powerpc32, and sparc32): it
     issues __NR_fstat64.

Also, two special cases requires specific implementations:

  1. alpha: it requires to handle _STAT_VER_KERNEL64 to issues
     __NR_fstat64 and use the kernel_stat with __NR_fstat otherwise.

  2. mips64: as for non-LFS implementation its ABIs differ from
     glibc exported one, which requires an specific conversion
     function to handle the kernel_stat.

Checked with a build for all affected ABIs. I also ch

sysdeps/unix/sysv/linux/fxstat64.c
@@ -16,15 +16,13 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <stddef.h>
-#include <fcntl.h>
+#define __fxstat __redirect___fxstat
 #include <sys/stat.h>
+#undef __fxstat
+#include <fcntl.h>
 #include <kernel_stat.h>
-
 #include <sysdep.h>
-#include <sys/syscall.h>
-
+#include <xstatconv.h>
 #include <statx_cp.h>
 
 /* Get information about the file FD in BUF.  */
@@ -32,17 +30,27 @@
 int
 ___fxstat64 (int vers, int fd, struct stat64 *buf)
 {
-  int result;
-#ifdef __NR_fstat64
-  result = INLINE_SYSCALL (fstat64, 2, fd, buf);
-#else
+#if XSTAT_IS_XSTAT64
+# ifdef __NR_fstat
+  /* 64-bit kABI, e.g. aarch64, ia64, powerpc64*, s390x, sparc64, riscv64,
+     and x86_64.  */
+  if (vers == _STAT_VER_KERNEL || vers == _STAT_VER_LINUX)
+    return INLINE_SYSCALL_CALL (fstat, fd, buf);
+  return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
+# else
+  /* New 32-bit kABIs with only 64-bit time_t support, e.g. arc, riscv32.  */
   struct statx tmp;
-  result = INLINE_SYSCALL (statx, 5, fd, "", AT_EMPTY_PATH, STATX_BASIC_STATS,
-                           &tmp);
-  if (result == 0)
+  int r = INLINE_SYSCALL_CALL (statx, fd, "", AT_EMPTY_PATH,
+			       STATX_BASIC_STATS, &tmp);
+  if (r == 0)
     __cp_stat64_statx (buf, &tmp);
-#endif
-  return result;
+  return r;
+# endif
+#else
+  /* All kABIs with non-LFS support, e.g. arm, csky, i386, hppa, m68k,
+     microblaze, mips32, nios2, sh, powerpc32, and sparc32.  */
+  return INLINE_SYSCALL_CALL (fstat64, fd, buf);
+#endif /* XSTAT_IS_XSTAT64  */
 }
 
 #include <shlib-compat.h>
@@ -55,4 +63,9 @@ hidden_ver (___fxstat64, __fxstat64)
 #else
 strong_alias (___fxstat64, __fxstat64)
 hidden_def (__fxstat64)
+#endif
+
+#if XSTAT_IS_XSTAT64
+strong_alias (__fxstat64, __fxstat);
+hidden_ver (__fxstat64, __fxstat)
 #endif

iikrllx/glibc-with-shred

    1.1. 64-bit kABI (aarch64, ia64, powerpc64*, s390x, riscv64, and
	 x86_64): it issuess __NR_fstat for _STAT_VER_KERNEL or
	 _STAT_VER_LINUX.
    1.2. Old 64-bit kABI with defines __NR_fstat64 instead of __NR_fstat
         (sparc64): it issues __NR_fstat for _STAT_VER_KERNEL or
         __NR_fstat64 and convert to struct stat64.
    1.3. New 32-bit kABIs with only 64-bit time_t support (arc and
Reviewed-by: Lukasz Majewski <lukma@denx.de>

sysdeps/unix/sysv/linux/fxstat64.c
@@ -16,15 +16,13 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <stddef.h>
-#include <fcntl.h>
+#define __fxstat __redirect___fxstat
 #include <sys/stat.h>
+#undef __fxstat
+#include <fcntl.h>
 #include <kernel_stat.h>
-
 #include <sysdep.h>
-#include <sys/syscall.h>
-
+#include <xstatconv.h>
 #include <statx_cp.h>
 
 /* Get information about the file FD in BUF.  */
@@ -32,17 +30,37 @@
 int
 ___fxstat64 (int vers, int fd, struct stat64 *buf)
 {
-  int result;
-#ifdef __NR_fstat64
-  result = INLINE_SYSCALL (fstat64, 2, fd, buf);
-#else
+#if XSTAT_IS_XSTAT64
+# ifdef __NR_fstat64
+  /* 64-bit kABI outlier, e.g. sparc64.  */
+  if (vers == _STAT_VER_KERNEL)
+    return INLINE_SYSCALL_CALL (fstat, fd, buf);
+  else
+    {
+      struct stat64 st64;
+      int r = INLINE_SYSCALL_CALL (fstat64, fd, &st64);
+      return r ?: __xstat32_conv (vers, &st64, (struct stat *) buf);
+    }
+# elif defined __NR_fstat
+  /* 64-bit kABI, e.g. aarch64, ia64, powerpc64*, s390x, riscv64,
+     and x86_64.  */
+  if (vers == _STAT_VER_KERNEL || vers == _STAT_VER_LINUX)
+    return INLINE_SYSCALL_CALL (fstat, fd, buf);
+  return INLINE_SYSCALL_ERROR_RETURN_VALUE (EINVAL);
+# else
+  /* New 32-bit kABIs with only 64-bit time_t support, e.g. arc, riscv32.  */
   struct statx tmp;
-  result = INLINE_SYSCALL (statx, 5, fd, "", AT_EMPTY_PATH, STATX_BASIC_STATS,
-                           &tmp);
-  if (result == 0)
+  int r = INLINE_SYSCALL_CALL (statx, fd, "", AT_EMPTY_PATH,
+			       STATX_BASIC_STATS, &tmp);
+  if (r == 0)
     __cp_stat64_statx (buf, &tmp);
-#endif
-  return result;
+  return r;
+# endif
+#else
+  /* All kABIs with non-LFS support, e.g. arm, csky, i386, hppa, m68k,
+     microblaze, mips32, nios2, sh, powerpc32, and sparc32.  */
+  return INLINE_SYSCALL_CALL (fstat64, fd, buf);
+#endif /* XSTAT_IS_XSTAT64  */
 }
 
 #include <shlib-compat.h>
@@ -55,4 +73,9 @@ hidden_ver (___fxstat64, __fxstat64)
 #else
 strong_alias (___fxstat64, __fxstat64)
 hidden_def (__fxstat64)
+#endif
+
+#if XSTAT_IS_XSTAT64
+strong_alias (__fxstat64, __fxstat);
+hidden_ver (__fxstat64, __fxstat)
 #endif