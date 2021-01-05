bminor/glibc

commit 09153638cfef9166586b0c21e852ea0d6f15a0fd

alpha: Set wait4 as cancellation entrypoint
Since both wait and waitpid are implemented on top of wait4.  It fixes
nptl/tst-cancel{x}{4,5,7}.

Checked on alpha-linux-gnu.

sysdeps/unix/sysv/linux/alpha/wait4.c
@@ -1,0 +1,26 @@
+/* wait4 -- wait for process to change state.  Linux/Alpha version.
+   Copyright (C) 2019 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <shlib-compat.h>
+
+#undef weak_alias
+#define weak_alias(a, b)
+#include <sysdeps/unix/sysv/linux/wait4.c>
+versioned_symbol (libc, __wait4, wait4, GLIBC_2_1);
+
+/* GLIBC_2_0 version is implemented at osf_wait4.c.  */

sysdeps/unix/sysv/linux/alpha/syscalls.list
@@ -28,7 +28,6 @@ getitimer	-	getitimer	i:ip	__getitimer	getitimer@@GLIBC_2.1
 setitimer	-	setitimer	i:ipP	__setitimer	setitimer@@GLIBC_2.1
 utimes		-	utimes		i:sp	__utimes	utimes@@GLIBC_2.1
 getrusage	-	getrusage	i:ip	__getrusage	getrusage@@GLIBC_2.1
-wait4		-	wait4		i:iWiP	__wait4		wait4@@GLIBC_2.1
 
 # avoid 64-bit aliases on 32-bit statfs syscalls
 fstatfs		-	fstatfs		i:ip	__fstatfs	fstatfs

iikrllx/glibc-with-shred

commit 15bbe7125fdfd0c292a983783381ebd2eb79e4fa

alpha: Provide wait4 for static library [BZ #27150]
This ia regression from 09153638cfef91, versioned_symbol acts as
weak_alias for !SHARED but it is undefined to avoid non versioned
alias from the generic implementation.

Checked with a build for alpha-linux-gnu.

sysdeps/unix/sysv/linux/alpha/wait4.c
@@ -21,6 +21,8 @@
 #undef weak_alias
 #define weak_alias(a, b)
 #include <sysdeps/unix/sysv/linux/wait4.c>
+#undef weak_alias
+#define weak_alias(name, aliasname) _weak_alias (name, aliasname)
 versioned_symbol (libc, __wait4, wait4, GLIBC_2_1);
 
 /* GLIBC_2_0 version is implemented at osf_wait4.c.  */