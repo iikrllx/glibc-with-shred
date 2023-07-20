bminor/glibc

commit 5324d258427fd11ca0f4f595c94016e568b26d6b

fileops: Don't process ,ccs= as individual mode flags (BZ#18906)
In processing the first 7 individual characters of the mode for fopen
if ,ccs= is used those characters will be processed as well.  Stop
processing individual mode flags once a comma is encountered.  This has
the effect of requiring ,ccs= to be the last mode flag in the mode
string.  Add a testcase to check that the ,ccs= mode flag is not
processed as individual mode flags.

Reviewed-by: DJ Delorie <dj@redhat.com>

libio/tst-fopenloc.c
@@ -17,6 +17,7 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include <errno.h>
+#include <fcntl.h>
 #include <locale.h>
 #include <mcheck.h>
 #include <stdio.h>
@@ -24,6 +25,7 @@
 #include <string.h>
 #include <wchar.h>
 #include <sys/resource.h>
+#include <support/check.h>
 #include <support/support.h>
 #include <support/xstdio.h>
 
@@ -48,11 +50,38 @@ do_bz17916 (void)
   if (fp != NULL)
     {
       printf ("unexpected success\n");
+      free (ccs);
+      fclose (fp);
       return 1;
     }
+
   free (ccs);
 
   return 0;
+}
+
+static int
+do_bz18906 (void)
+{
+  /* BZ #18906 -- check processing of ,ccs= as flags case.  */
+
+  const char *ccs = "r,ccs=+ISO-8859-1";
+  size_t retval;
+
+  FILE *fp = fopen (inputfile, ccs);
+  int flags;
+
+  TEST_VERIFY (fp != NULL);
+
+  if (fp != NULL)
+    {
+      flags = fcntl (fileno (fp), F_GETFL);
+      retval = (flags & O_RDWR) | (flags & O_WRONLY);
+      TEST_COMPARE (retval, false);
+      fclose (fp);
+    }
+
+  return EXIT_SUCCESS;
 }
 
 static int
@@ -78,7 +107,10 @@ do_test (void)
 
   xfclose (fp);
 
-  return do_bz17916 ();
+  TEST_COMPARE (do_bz17916 (), 0);
+  TEST_COMPARE (do_bz18906 (), 0);
+
+  return EXIT_SUCCESS;
 }
 
 #include <support/test-driver.c>

libio/fileops.c
@@ -247,6 +247,7 @@ _IO_new_file_fopen (FILE *fp, const char *filename, const char *mode,
       switch (*++mode)
 	{
 	case '\0':
+	case ',':
 	  break;
 	case '+':
 	  omode = O_RDWR;

iikrllx/glibc-with-shred

commit bceefb01e0d94515aef24c3012034e67297b6f4b

hurd: Fix tst-openloc
On GNU/Hurd, O_RDWR actually is O_WRONLY|O_RDONLY, so checking through
bitness really is wrong. O_ACCMODE is there for this.

Fixes: 5324d258427f ("fileops: Don't process ,ccs= as individual mode flags (BZ#18906)")

libio/tst-fopenloc.c
@@ -76,7 +76,8 @@ do_bz18906 (void)
   if (fp != NULL)
     {
       flags = fcntl (fileno (fp), F_GETFL);
-      retval = (flags & O_RDWR) | (flags & O_WRONLY);
+      retval = ((flags & O_ACCMODE) == O_RDWR);
+      retval |= ((flags & O_ACCMODE) == O_WRONLY);
       TEST_COMPARE (retval, false);
       fclose (fp);
     }