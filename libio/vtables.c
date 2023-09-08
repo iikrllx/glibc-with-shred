bminor/glibc

commit 3020f72618e4f1d7338cd42b8bc7b2813e961b5a

libio: Remove the usage of __libc_IO_vtables
Instead of using a special ELF section along with a linker script
directive to put the IO vtables within the RELRO section, the libio
vtables are all moved to an array marked as data.relro (so linker
will place in the RELRO segment without the need of extra directives).

To avoid static linking namespace issues and including all vtable
referenced objects, all required function pointers are set to weak alias.

Checked on x86_64-linux-gnu, i686-linux-gnu, and aarch64-linux-gnu.

Reviewed-by: Carlos O'Donell <carlos@redhat.com>

libio/vtables.c
@@ -21,6 +21,472 @@
 #include <stdio.h>
 #include <ldsodefs.h>
 #include <pointer_guard.h>
+#include <libio-macros.h>
+
+/* Both _IO_str_* and _IO_new_file functions are pulled into every link (from
+   stdio initialization).  */
+#ifndef SHARED
+/* NB: the following directives do add pragma weak for _IO_default _* and
+   _IO_wdefault_* symbols to potentially avoid link failures, since they
+   are always used when the __io_vtables is used.  */
+# pragma weak _IO_wstr_finish
+# pragma weak _IO_wstr_overflow
+# pragma weak _IO_wstr_pbackfail
+# pragma weak _IO_wstr_seekoff
+# pragma weak _IO_wstr_underflow
+
+# pragma weak _IO_file_close
+# pragma weak _IO_file_close_mmap
+# pragma weak _IO_file_doallocate
+# pragma weak _IO_file_finish
+# pragma weak _IO_file_jumps_alias
+# pragma weak _IO_file_overflow
+# pragma weak _IO_file_read
+# pragma weak _IO_file_seek
+# pragma weak _IO_file_seekoff_maybe_mmap
+# pragma weak _IO_file_seekoff_mmap
+# pragma weak _IO_file_setbuf
+# pragma weak _IO_file_setbuf_mmap
+# pragma weak _IO_file_setbuf_mmap
+# pragma weak _IO_file_stat
+# pragma weak _IO_file_sync
+# pragma weak _IO_file_sync_mmap
+# pragma weak _IO_file_underflow
+# pragma weak _IO_file_underflow_maybe_mmap
+# pragma weak _IO_file_underflow_mmap
+# pragma weak _IO_file_xsgetn
+# pragma weak _IO_file_xsgetn_maybe_mmap
+# pragma weak _IO_file_xsgetn_mmap
+# pragma weak _IO_file_xsputn
+
+# pragma weak _IO_wfile_overflow
+# pragma weak _IO_wfile_sync
+# pragma weak _IO_wfile_underflow
+# pragma weak _IO_wfile_underflow_maybe_mmap
+# pragma weak _IO_wfile_underflow_mmap
+# pragma weak _IO_wfile_doallocate
+# pragma weak _IO_wfile_jumps_alia
+# pragma weak _IO_wfile_seekoff
+# pragma weak _IO_wfile_xsputn
+
+# pragma weak _IO_new_proc_close
+
+# pragma weak _IO_cookie_close
+# pragma weak _IO_cookie_read
+# pragma weak _IO_cookie_seek
+# pragma weak _IO_cookie_seekoff
+# pragma weak _IO_cookie_write
+
+# pragma weak _IO_mem_finish
+# pragma weak _IO_mem_sync
+
+# pragma weak _IO_wmem_finish
+# pragma weak _IO_wmem_sync
+
+# pragma weak __printf_buffer_as_file_overflow
+# pragma weak __printf_buffer_as_file_xsputn
+
+# pragma weak __wprintf_buffer_as_file_overflow
+# pragma weak __wprintf_buffer_as_file_xsputn
+#endif
+
+const struct _IO_jump_t __io_vtables[IO_VTABLES_LEN] attribute_relro =
+{
+  /* _IO_str_jumps  */
+  [IO_STR_JUMPS] =
+  {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_str_finish),
+    JUMP_INIT (overflow, _IO_str_overflow),
+    JUMP_INIT (underflow, _IO_str_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_str_pbackfail),
+    JUMP_INIT (xsputn, _IO_default_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_str_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_default_setbuf),
+    JUMP_INIT (sync, _IO_default_sync),
+    JUMP_INIT (doallocate, _IO_default_doallocate),
+    JUMP_INIT (read, _IO_default_read),
+    JUMP_INIT (write, _IO_default_write),
+    JUMP_INIT (seek, _IO_default_seek),
+    JUMP_INIT (close, _IO_default_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_wstr_jumps  */
+  [IO_WSTR_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_wstr_finish),
+    JUMP_INIT (overflow, (_IO_overflow_t) _IO_wstr_overflow),
+    JUMP_INIT (underflow, (_IO_underflow_t) _IO_wstr_underflow),
+    JUMP_INIT (uflow, (_IO_underflow_t) _IO_wdefault_uflow),
+    JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wstr_pbackfail),
+    JUMP_INIT (xsputn, _IO_wdefault_xsputn),
+    JUMP_INIT (xsgetn, _IO_wdefault_xsgetn),
+    JUMP_INIT (seekoff, _IO_wstr_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_default_setbuf),
+    JUMP_INIT (sync, _IO_default_sync),
+    JUMP_INIT (doallocate, _IO_wdefault_doallocate),
+    JUMP_INIT (read, _IO_default_read),
+    JUMP_INIT (write, _IO_default_write),
+    JUMP_INIT (seek, _IO_default_seek),
+    JUMP_INIT (close, _IO_default_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_file_jumps  */
+  [IO_FILE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_file_finish),
+    JUMP_INIT (overflow, _IO_file_overflow),
+    JUMP_INIT (underflow, _IO_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn),
+    JUMP_INIT (seekoff, _IO_new_file_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_new_file_setbuf),
+    JUMP_INIT (sync, _IO_new_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_file_jumps_mmap  */
+  [IO_FILE_JUMPS_MMAP] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_file_finish),
+    JUMP_INIT (overflow, _IO_file_overflow),
+    JUMP_INIT (underflow, _IO_file_underflow_mmap),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_new_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn_mmap),
+    JUMP_INIT (seekoff, _IO_file_seekoff_mmap),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, (_IO_setbuf_t) _IO_file_setbuf_mmap),
+    JUMP_INIT (sync, _IO_file_sync_mmap),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close_mmap),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_file_jumps_maybe_mmap  */
+  [IO_FILE_JUMPS_MAYBE_MMAP] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_file_finish),
+    JUMP_INIT (overflow, _IO_file_overflow),
+    JUMP_INIT (underflow, _IO_file_underflow_maybe_mmap),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_new_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn_maybe_mmap),
+    JUMP_INIT (seekoff, _IO_file_seekoff_maybe_mmap),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, (_IO_setbuf_t) _IO_file_setbuf_mmap),
+    JUMP_INIT (sync, _IO_new_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_wfile_jumps  */
+  [IO_WFILE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_new_file_finish),
+    JUMP_INIT (overflow, (_IO_overflow_t) _IO_wfile_overflow),
+    JUMP_INIT (underflow, (_IO_underflow_t) _IO_wfile_underflow),
+    JUMP_INIT (uflow, (_IO_underflow_t) _IO_wdefault_uflow),
+    JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wdefault_pbackfail),
+    JUMP_INIT (xsputn, _IO_wfile_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn),
+    JUMP_INIT (seekoff, _IO_wfile_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_new_file_setbuf),
+    JUMP_INIT (sync, (_IO_sync_t) _IO_wfile_sync),
+    JUMP_INIT (doallocate, _IO_wfile_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_wfile_jumps_mmap  */
+  [IO_WFILE_JUMPS_MMAP] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_new_file_finish),
+    JUMP_INIT (overflow, (_IO_overflow_t) _IO_wfile_overflow),
+    JUMP_INIT (underflow, (_IO_underflow_t) _IO_wfile_underflow_mmap),
+    JUMP_INIT (uflow, (_IO_underflow_t) _IO_wdefault_uflow),
+    JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wdefault_pbackfail),
+    JUMP_INIT (xsputn, _IO_wfile_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn),
+    JUMP_INIT (seekoff, _IO_wfile_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_file_setbuf_mmap),
+    JUMP_INIT (sync, (_IO_sync_t) _IO_wfile_sync),
+    JUMP_INIT (doallocate, _IO_wfile_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close_mmap),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_wfile_jumps_maybe_mmap  */
+  [IO_WFILE_JUMPS_MAYBE_MMAP] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_new_file_finish),
+    JUMP_INIT (overflow, (_IO_overflow_t) _IO_wfile_overflow),
+    JUMP_INIT (underflow, (_IO_underflow_t) _IO_wfile_underflow_maybe_mmap),
+    JUMP_INIT (uflow, (_IO_underflow_t) _IO_wdefault_uflow),
+    JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wdefault_pbackfail),
+    JUMP_INIT (xsputn, _IO_wfile_xsputn),
+    JUMP_INIT (xsgetn, _IO_file_xsgetn),
+    JUMP_INIT (seekoff, _IO_wfile_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_file_setbuf_mmap),
+    JUMP_INIT (sync, (_IO_sync_t) _IO_wfile_sync),
+    JUMP_INIT (doallocate, _IO_wfile_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_cookie_jumps  */
+  [IO_COOKIE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_file_finish),
+    JUMP_INIT (overflow, _IO_file_overflow),
+    JUMP_INIT (underflow, _IO_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_cookie_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_file_setbuf),
+    JUMP_INIT (sync, _IO_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_cookie_read),
+    JUMP_INIT (write, _IO_cookie_write),
+    JUMP_INIT (seek, _IO_cookie_seek),
+    JUMP_INIT (close, _IO_cookie_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue),
+  },
+  /* _IO_proc_jumps  */
+  [IO_PROC_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_new_file_finish),
+    JUMP_INIT (overflow, _IO_new_file_overflow),
+    JUMP_INIT (underflow, _IO_new_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_new_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_new_file_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_new_file_setbuf),
+    JUMP_INIT (sync, _IO_new_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_new_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_new_proc_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_mem_jumps  */
+  [IO_MEM_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_mem_finish),
+    JUMP_INIT (overflow, _IO_str_overflow),
+    JUMP_INIT (underflow, _IO_str_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_str_pbackfail),
+    JUMP_INIT (xsputn, _IO_default_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_str_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_default_setbuf),
+    JUMP_INIT (sync, _IO_mem_sync),
+    JUMP_INIT (doallocate, _IO_default_doallocate),
+    JUMP_INIT (read, _IO_default_read),
+    JUMP_INIT (write, _IO_default_write),
+    JUMP_INIT (seek, _IO_default_seek),
+    JUMP_INIT (close, _IO_default_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  /* _IO_wmem_jumps  */
+  [IO_WMEM_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_wmem_finish),
+    JUMP_INIT (overflow, (_IO_overflow_t) _IO_wstr_overflow),
+    JUMP_INIT (underflow, (_IO_underflow_t) _IO_wstr_underflow),
+    JUMP_INIT (uflow, (_IO_underflow_t) _IO_wdefault_uflow),
+    JUMP_INIT (pbackfail, (_IO_pbackfail_t) _IO_wstr_pbackfail),
+    JUMP_INIT (xsputn, _IO_wdefault_xsputn),
+    JUMP_INIT (xsgetn, _IO_wdefault_xsgetn),
+    JUMP_INIT (seekoff, _IO_wstr_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_default_setbuf),
+    JUMP_INIT (sync, _IO_wmem_sync),
+    JUMP_INIT (doallocate, _IO_wdefault_doallocate),
+    JUMP_INIT (read, _IO_default_read),
+    JUMP_INIT (write, _IO_default_write),
+    JUMP_INIT (seek, _IO_default_seek),
+    JUMP_INIT (close, _IO_default_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+  [IO_PRINTF_BUFFER_AS_FILE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, NULL),
+    JUMP_INIT (overflow, __printf_buffer_as_file_overflow),
+    JUMP_INIT (underflow, NULL),
+    JUMP_INIT (uflow, NULL),
+    JUMP_INIT (pbackfail, NULL),
+    JUMP_INIT (xsputn, __printf_buffer_as_file_xsputn),
+    JUMP_INIT (xsgetn, NULL),
+    JUMP_INIT (seekoff, NULL),
+    JUMP_INIT (seekpos, NULL),
+    JUMP_INIT (setbuf, NULL),
+    JUMP_INIT (sync, NULL),
+    JUMP_INIT (doallocate, NULL),
+    JUMP_INIT (read, NULL),
+    JUMP_INIT (write, NULL),
+    JUMP_INIT (seek, NULL),
+    JUMP_INIT (close, NULL),
+    JUMP_INIT (stat, NULL),
+    JUMP_INIT (showmanyc, NULL),
+    JUMP_INIT (imbue, NULL)
+  },
+  [IO_WPRINTF_BUFFER_AS_FILE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, NULL),
+    JUMP_INIT (overflow, (_IO_overflow_t) __wprintf_buffer_as_file_overflow),
+    JUMP_INIT (underflow, NULL),
+    JUMP_INIT (uflow, NULL),
+    JUMP_INIT (pbackfail, NULL),
+    JUMP_INIT (xsputn, __wprintf_buffer_as_file_xsputn),
+    JUMP_INIT (xsgetn, NULL),
+    JUMP_INIT (seekoff, NULL),
+    JUMP_INIT (seekpos, NULL),
+    JUMP_INIT (setbuf, NULL),
+    JUMP_INIT (sync, NULL),
+    JUMP_INIT (doallocate, NULL),
+    JUMP_INIT (read, NULL),
+    JUMP_INIT (write, NULL),
+    JUMP_INIT (seek, NULL),
+    JUMP_INIT (close, NULL),
+    JUMP_INIT (stat, NULL),
+    JUMP_INIT (showmanyc, NULL),
+    JUMP_INIT (imbue, NULL)
+  },
+
+#if SHLIB_COMPAT (libc, GLIBC_2_0, GLIBC_2_1)
+  /* _IO_old_file_jumps  */
+  [IO_OLD_FILE_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_old_file_finish),
+    JUMP_INIT (overflow, _IO_old_file_overflow),
+    JUMP_INIT (underflow, _IO_old_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_old_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_old_file_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_old_file_setbuf),
+    JUMP_INIT (sync, _IO_old_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_old_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_file_close),
+    JUMP_INIT (stat, _IO_file_stat)
+  },
+  /*  _IO_old_proc_jumps  */
+  [IO_OLD_PROC_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_old_file_finish),
+    JUMP_INIT (overflow, _IO_old_file_overflow),
+    JUMP_INIT (underflow, _IO_old_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_old_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_old_file_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_old_file_setbuf),
+    JUMP_INIT (sync, _IO_old_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_file_read),
+    JUMP_INIT (write, _IO_old_file_write),
+    JUMP_INIT (seek, _IO_file_seek),
+    JUMP_INIT (close, _IO_old_proc_close),
+    JUMP_INIT (stat, _IO_file_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue)
+  },
+#endif
+
+#if SHLIB_COMPAT (libc, GLIBC_2_0, GLIBC_2_2)
+  /* _IO_old_cookie_jumps  */
+  [IO_OLD_COOKIED_JUMPS] = {
+    JUMP_INIT_DUMMY,
+    JUMP_INIT (finish, _IO_file_finish),
+    JUMP_INIT (overflow, _IO_file_overflow),
+    JUMP_INIT (underflow, _IO_file_underflow),
+    JUMP_INIT (uflow, _IO_default_uflow),
+    JUMP_INIT (pbackfail, _IO_default_pbackfail),
+    JUMP_INIT (xsputn, _IO_file_xsputn),
+    JUMP_INIT (xsgetn, _IO_default_xsgetn),
+    JUMP_INIT (seekoff, _IO_cookie_seekoff),
+    JUMP_INIT (seekpos, _IO_default_seekpos),
+    JUMP_INIT (setbuf, _IO_file_setbuf),
+    JUMP_INIT (sync, _IO_file_sync),
+    JUMP_INIT (doallocate, _IO_file_doallocate),
+    JUMP_INIT (read, _IO_cookie_read),
+    JUMP_INIT (write, _IO_cookie_write),
+    JUMP_INIT (seek, _IO_old_cookie_seek),
+    JUMP_INIT (close, _IO_cookie_close),
+    JUMP_INIT (stat, _IO_default_stat),
+    JUMP_INIT (showmanyc, _IO_default_showmanyc),
+    JUMP_INIT (imbue, _IO_default_imbue),
+  },
+#endif
+};
 
 #ifdef SHARED
 
@@ -82,3 +548,15 @@ check_stdfiles_vtables (void)
     IO_set_accept_foreign_vtables (&_IO_vtable_check);
 }
 #endif
+
+#define STR(s)  XSTR(s)
+#define XSTR(s) #s
+
+#undef _IO_file_jumps
+#define _IO_file_jumps_alias  "__io_vtables + " STR(IO_FILE_JUMPS_OFFSET)
+declare_object_symbol_alias (_IO_file_jumps, _IO_file_jumps_alias,
+			     IO_JUMP_T_SIZE)
+#undef _IO_wfile_jumps
+#define _IO_wfile_jumps_alias  "__io_vtables + " STR(IO_WFILE_JUMPS_OFFSET)
+declare_object_symbol_alias (_IO_wfile_jumps, _IO_wfile_jumps_alias,
+			     IO_JUMP_T_SIZE)

Makerules
@@ -544,34 +544,10 @@ $(LINK.o) -shared -static-libgcc -Wl,-O1 $(sysdep-LDFLAGS) \
 	  -L$(subst :, -L,$(rpath-link)) -Wl,-rpath-link=$(rpath-link)
 endef
 
-ifeq (yes,$(use-default-link))
 # If the linker is good enough, we can let it use its default linker script.
 # In the long term the custom linker script will be removed.
 shlib-lds =
 shlib-lds-flags =
-else
-# binutils only position loadable notes into the first page for binaries,
-# not for shared objects
-# lld --verbose does not dump a linker script.  Use -fuse-ld=bfd.
-$(common-objpfx)shlib.lds: $(common-objpfx)config.make $(..)Makerules
-	$(LINK.o) -shared -Wl,-O1 \
-		  -nostdlib -nostartfiles -fuse-ld=bfd \
-		  $(sysdep-LDFLAGS) $(rtld-LDFLAGS) $(LDFLAGS.so) \
-		  -Wl,--verbose 2>/dev/null | \
-	  sed > $@T \
-	      -e '/^=========/,/^=========/!d;/^=========/d' \
-	      -e 's@^.*\*(\.jcr).*$$@& \
-		 PROVIDE(__start___libc_IO_vtables = .);\
-		 __libc_IO_vtables : { *(__libc_IO_vtables) }\
-		 PROVIDE(__stop___libc_IO_vtables = .);\
-		 /DISCARD/ : { *(.gnu.glibc-stub.*) }@'
-	test -s $@T
-	mv -f $@T $@
-common-generated += shlib.lds
-
-shlib-lds = $(common-objpfx)shlib.lds
-shlib-lds-flags = -T $(shlib-lds)
-endif
 
 define build-shlib
 $(build-shlib-helper) -o $@ $(shlib-lds-flags) \

elf/Makefile
@@ -595,23 +595,9 @@ $(objpfx)tst-relro-ldso.out: tst-relro-symbols.py $(..)/scripts/glibcelf.py \
 $(objpfx)tst-relro-libc.out: tst-relro-symbols.py $(..)/scripts/glibcelf.py \
   $(common-objpfx)libc.so
 	$(PYTHON) tst-relro-symbols.py $(common-objpfx)libc.so \
-	    --required=_IO_cookie_jumps \
 	    --required=_IO_file_jumps \
-	    --required=_IO_file_jumps_maybe_mmap \
-	    --required=_IO_file_jumps_mmap \
-	    --required=_IO_mem_jumps \
-	    --required=_IO_printf_buffer_as_file_jumps \
-	    --required=_IO_proc_jumps \
-	    --required=_IO_str_jumps \
 	    --required=_IO_wfile_jumps \
-	    --required=_IO_wfile_jumps_maybe_mmap \
-	    --required=_IO_wfile_jumps_mmap \
-	    --required=_IO_wmem_jumps \
-	    --required=_IO_wprintf_buffer_as_file_jumps \
-	    --required=_IO_wstr_jumps \
-	    --optional=_IO_old_cookie_jumps \
-	    --optional=_IO_old_file_jumps \
-	    --optional=_IO_old_proc_jumps \
+	    --required=__io_vtables \
 	  > $@ 2>&1; $(evaluate-test)
 
 ifeq ($(run-built-tests),yes)

include/libc-symbols.h
@@ -243,20 +243,26 @@ for linking")
    This is only necessary when defining something in assembly, or playing
    funny alias games where the size should be other than what the compiler
    thinks it is.  */
-#ifdef __ASSEMBLER__
-# define declare_object_symbol_alias(symbol, original, size) \
+#define declare_object_symbol_alias(symbol, original, size) \
   declare_object_symbol_alias_1 (symbol, original, size)
+#ifdef __ASSEMBLER__
 # define declare_object_symbol_alias_1(symbol, original, s_size) \
    strong_alias (original, symbol) ASM_LINE_SEP \
    .type C_SYMBOL_NAME (symbol), %object ASM_LINE_SEP \
    .size C_SYMBOL_NAME (symbol), s_size ASM_LINE_SEP
 #else /* Not __ASSEMBLER__.  */
 # ifdef HAVE_ASM_SET_DIRECTIVE
-#  define declare_symbol_alias_1_alias(symbol, original) \
-     ".set " __SYMBOL_PREFIX #symbol ", " __SYMBOL_PREFIX #original
+#  define declare_object_symbol_alias_1(symbol, original, size) \
+     asm (".global " __SYMBOL_PREFIX # symbol "\n" \
+	  ".type " __SYMBOL_PREFIX # symbol ", %object\n" \
+	  ".set " __SYMBOL_PREFIX #symbol ", " __SYMBOL_PREFIX original "\n" \
+	  ".size " __SYMBOL_PREFIX #symbol ", " #size "\n");
 # else
-#  define declare_symbol_alias_1_alias(symbol, original) \
-     __SYMBOL_PREFIX #symbol " = " __SYMBOL_PREFIX #original
+#  define declare_object_symbol_alias_1(symbol, original, size) \
+     asm (".global " __SYMBOL_PREFIX # symbol "\n" \
+	  ".type " __SYMBOL_PREFIX # symbol ", %object\n" \
+	  __SYMBOL_PREFIX #symbol " = " __SYMBOL_PREFIX original "\n" \
+	  ".size " __SYMBOL_PREFIX #symbol ", " #size "\n");
 # endif /* HAVE_ASM_SET_DIRECTIVE */
 #endif /* __ASSEMBLER__ */
 

libio/Makefile
@@ -51,6 +51,8 @@ routines	:=							      \
 									      \
 	libc_fatal fmemopen oldfmemopen vtables
 
+gen-as-const-headers += libio-macros.sym
+
 tests = tst_swprintf tst_wprintf tst_swscanf tst_wscanf tst_getwc tst_putwc   \
 	tst_wprintf2 tst-widetext test-fmemopen tst-ext tst-ext2 \
 	tst-fgetws tst-ungetwc1 tst-ungetwc2 tst-swscanf tst-sscanf	      \

libio/fileops.c
@@ -815,7 +815,7 @@ _IO_new_file_sync (FILE *fp)
 }
 libc_hidden_ver (_IO_new_file_sync, _IO_file_sync)
 
-static int
+int
 _IO_file_sync_mmap (FILE *fp)
 {
   if (fp->_IO_read_ptr != fp->_IO_read_end)
@@ -1109,7 +1109,7 @@ _IO_file_seekoff_mmap (FILE *fp, off64_t offset, int dir, int mode)
   return offset;
 }
 
-static off64_t
+off64_t
 _IO_file_seekoff_maybe_mmap (FILE *fp, off64_t offset, int dir,
 			     int mode)
 {
@@ -1360,7 +1360,7 @@ _IO_file_xsgetn (FILE *fp, void *data, size_t n)
 }
 libc_hidden_def (_IO_file_xsgetn)
 
-static size_t
+size_t
 _IO_file_xsgetn_mmap (FILE *fp, void *data, size_t n)
 {
   size_t have;
@@ -1405,7 +1405,7 @@ _IO_file_xsgetn_mmap (FILE *fp, void *data, size_t n)
   return s - (char *) data;
 }
 
-static size_t
+size_t
 _IO_file_xsgetn_maybe_mmap (FILE *fp, void *data, size_t n)
 {
   /* We only get here if this is the first attempt to read something.
@@ -1428,76 +1428,3 @@ versioned_symbol (libc, _IO_new_file_seekoff, _IO_file_seekoff, GLIBC_2_1);
 versioned_symbol (libc, _IO_new_file_underflow, _IO_file_underflow, GLIBC_2_1);
 versioned_symbol (libc, _IO_new_file_write, _IO_file_write, GLIBC_2_1);
 versioned_symbol (libc, _IO_new_file_xsputn, _IO_file_xsputn, GLIBC_2_1);
-
-const struct _IO_jump_t _IO_file_jumps libio_vtable =
-{
-  JUMP_INIT_DUMMY,
-  JUMP_INIT(finish, _IO_file_finish),
-  JUMP_INIT(overflow, _IO_file_overflow),
-  JUMP_INIT(underflow, _IO_file_underflow),
-  JUMP_INIT(uflow, _IO_default_uflow),
-  JUMP_INIT(pbackfail, _IO_default_pbackfail),
-  JUMP_INIT(xsputn, _IO_file_xsputn),
-  JUMP_INIT(xsgetn, _IO_file_xsgetn),
-  JUMP_INIT(seekoff, _IO_new_file_seekoff),
-  JUMP_INIT(seekpos, _IO_default_seekpos),
-  JUMP_INIT(setbuf, _IO_new_file_setbuf),
-  JUMP_INIT(sync, _IO_new_file_sync),
-  JUMP_INIT(doallocate, _IO_file_doallocate),
-  JUMP_INIT(read, _IO_file_read),
-  JUMP_INIT(write, _IO_new_file_write),
-  JUMP_INIT(seek, _IO_file_seek),
-  JUMP_INIT(close, _IO_file_close),
-  JUMP_INIT(stat, _IO_file_stat),
-  JUMP_INIT(showmanyc, _IO_default_showmanyc),
-  JUMP_INIT(imbue, _IO_default_imbue)
-};
-libc_hidden_data_def (_IO_file_jumps)
-
-const struct _IO_jump_t _IO_file_jumps_mmap libio_vtable =
-{
-  JUMP_INIT_DUMMY,
-  JUMP_INIT(finish, _IO_file_finish),
-  JUMP_INIT(overflow, _IO_file_overflow),
-  JUMP_INIT(underflow, _IO_file_underflow_mmap),
-  JUMP_INIT(uflow, _IO_default_uflow),
-  JUMP_INIT(pbackfail, _IO_default_pbackfail),
-  JUMP_INIT(xsputn, _IO_new_file_xsputn),
-  JUMP_INIT(xsgetn, _IO_file_xsgetn_mmap),
-  JUMP_INIT(seekoff, _IO_file_seekoff_mmap),
-  JUMP_INIT(seekpos, _IO_default_seekpos),
-  JUMP_INIT(setbuf, (_IO_setbuf_t) _IO_file_setbuf_mmap),
-  JUMP_INIT(sync, _IO_file_sync_mmap),
-  JUMP_INIT(doallocate, _IO_file_doallocate),
-  JUMP_INIT(read, _IO_file_read),
-  JUMP_INIT(write, _IO_new_file_write),
-  JUMP_INIT(seek, _IO_file_seek),
-  JUMP_INIT(close, _IO_file_close_mmap),
-  JUMP_INIT(stat, _IO_file_stat),
-  JUMP_INIT(showmanyc, _IO_default_showmanyc),
-  JUMP_INIT(imbue, _IO_default_imbue)
-};
-
-const struct _IO_jump_t _IO_file_jumps_maybe_mmap libio_vtable =
-{
-  JUMP_INIT_DUMMY,
-  JUMP_INIT(finish, _IO_file_finish),
-  JUMP_INIT(overflow, _IO_file_overflow),
-  JUMP_INIT(underflow, _IO_file_underflow_maybe_mmap),
-  JUMP_INIT(uflow, _IO_default_uflow),
-  JUMP_INIT(pbackfail, _IO_default_pbackfail),
-  JUMP_INIT(xsputn, _IO_new_file_xsputn),
-  JUMP_INIT(xsgetn, _IO_file_xsgetn_maybe_mmap),
-  JUMP_INIT(seekoff, _IO_file_seekoff_maybe_mmap),
-  JUMP_INIT(seekpos, _IO_default_seekpos),
-  JUMP_INIT(setbuf, (_IO_setbuf_t) _IO_file_setbuf_mmap),
-  JUMP_INIT(sync, _IO_new_file_sync),
-  JUMP_INIT(doallocate, _IO_file_doallocate),
-  JUMP_INIT(read, _IO_file_read),
-  JUMP_INIT(write, _IO_new_file_write),
-  JUMP_INIT(seek, _IO_file_seek),
-  JUMP_INIT(close, _IO_file_close),
-  JUMP_INIT(stat, _IO_file_stat),
-  JUMP_INIT(showmanyc, _IO_default_showmanyc),
-  JUMP_INIT(imbue, _IO_default_imbue)
-};

iikrllx/glibc-with-shred

commit 45476c0f03c11c1acd98a91bf1387817ecc45695

libio: Fix oversized __io_vtables
IO_VTABLES_LEN is the size of the struct array in bytes, not the number
of __IO_jump_t's in the array. Drops just under 384kb from .rodata on
LP64 machines.

Fixes: 3020f72618e ("libio: Remove the usage of __libc_IO_vtables")
Signed-off-by: Adam Jackson <ajax@redhat.com>
Reviewed-by: Florian Weimer <fweimer@redhat.com>
Tested-by: Florian Weimer <fweimer@redhat.com>

libio/vtables.c
@@ -20,6 +20,7 @@
 #include <libioP.h>
 #include <stdio.h>
 #include <ldsodefs.h>
+#include <array_length.h>
 #include <pointer_guard.h>
 #include <libio-macros.h>
 
@@ -88,7 +89,7 @@
 # pragma weak __wprintf_buffer_as_file_xsputn
 #endif
 
-const struct _IO_jump_t __io_vtables[IO_VTABLES_LEN] attribute_relro =
+const struct _IO_jump_t __io_vtables[] attribute_relro =
 {
   /* _IO_str_jumps  */
   [IO_STR_JUMPS] =
@@ -485,6 +486,8 @@ const struct _IO_jump_t __io_vtables[IO_VTABLES_LEN] attribute_relro =
   },
 #endif
 };
+_Static_assert (array_length (__io_vtables) == IO_VTABLES_NUM,
+                "initializer count");
 
 #ifdef SHARED
 